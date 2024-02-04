bcb9c9f6c8f846b738203107ed6e03b3186d5ecd
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-02-04 13:25:06 +0100

    feat: button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center rounded-md transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-white hover:bg-primary/80',
+        secondary: 'border border-primary text-primary hover:bg-primary/80 hover:text-white',
+        destructive: 'bg-destructive text-white hover:bg-destructive/80',
+      },
+      size: {
+        default: 'h-10 py-2 px-4',
+        sm: 'h-9 px-3 text-sm',
+        lg: 'h-11 px-8',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, variant, size, ...props }, ref) => {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
