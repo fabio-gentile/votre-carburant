3ff0a5eadf51c5c16c662efb667c188998302c6d
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-02-18 13:15:14 +0100

    feat: format timestamp to hours minutes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function millisecondsToHoursAndMinutes(milliseconds: number): string {
+  const hours = Math.floor(milliseconds / (1000 * 60 * 60));
+  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+
+  let result = '';
+  if (hours > 0) {
+    result += hours + ' heure';
+    if (hours > 1) {
+      result += 's';
+    }
+    result += ' et ';
+  }
+  result += minutes + ' minute';
+  if (minutes > 1) {
+    result += 's';
+  }
+
+  return result;
+}
