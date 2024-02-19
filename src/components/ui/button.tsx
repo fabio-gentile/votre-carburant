f7e06843b79971b3c4af79562226b0e1d8d4da4d
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-02-19 14:01:46 +0100

    fix: border

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,12 +3,12 @@ import { VariantProps, cva } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  'inline-flex items-center justify-center border rounded-md transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
   {
     variants: {
       variant: {
         default: 'bg-primary text-white hover:bg-primary/80',
-        secondary: 'border border-primary text-primary hover:bg-primary/80 hover:text-white',
+        secondary: 'border-primary text-primary hover:bg-primary/80 hover:text-white',
         destructive: 'bg-destructive text-white hover:bg-destructive/80',
       },
       size: {
