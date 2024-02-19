7e16715549964d83f65e5794448b12bcac109086
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-02-19 20:45:43 +0100

    feat: about page

---
+++ src/app/a-propos/page.tsx
@@ -0,0 +1,27 @@
+import { PrimaryTitle } from '@/components/ui/title';
+
+export default async function Page() {
+  return (
+    <main className='flex flex-col justify-center gap-8 sm:gap-12 lg:gap-16'>
+      <PrimaryTitle>À propos</PrimaryTitle>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Faucibus scelerisque
+        eleifend donec pretium vulputate sapien nec sagittis. Varius vel pharetra vel turpis nunc. Consequat ac felis
+        donec et. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Euismod.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Faucibus scelerisque
+        eleifend donec pretium vulputate sapien nec sagittis. Varius vel pharetra vel turpis nunc. Consequat ac felis
+        donec et. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Euismod.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Faucibus scelerisque
+        eleifend donec pretium vulputate sapien nec sagittis. Varius vel pharetra vel turpis nunc. Consequat ac felis
+        donec et. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Euismod.
+      </p>
+    </main>
+  );
+}
