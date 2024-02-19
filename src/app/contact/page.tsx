0fddb9ac6493eba35b5511bdfa2f5e11bdc82eba
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-02-19 20:45:37 +0100

    feat: contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import { PrimaryTitle } from '@/components/ui/title';
+
+export default async function Page() {
+  return (
+    <main className='flex flex-col justify-center gap-8 sm:gap-12 lg:gap-16'>
+      <PrimaryTitle>Me contacter</PrimaryTitle>
+      <a
+        href='mailto:contact@votre-carburant.fr'
+        className='hover:underline'
+      >
+        Pour me contacter : contact@votre-carburant.fr
+      </a>
+    </main>
+  );
+}
