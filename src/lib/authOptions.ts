fad9d9edf9bc73a183a37d1a699b5dcbed541bdb
Fabio Gentile <117646999+fabio-gentile@users.noreply.github.com>
2024-02-19 23:00:01 +0100

    feat: middleware

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,50 +1,56 @@
 import { connectMongoDB } from '@/lib/mongodb';
 import User from '@/models/user';
 import GoogleProvider from 'next-auth/providers/google';
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      }),
-    ],
-    callbacks: {
-      async signIn({ user, account }) {
-        if (account?.provider === 'google') {
-          const { name, email } = user;
-          try {
-            await connectMongoDB();
-            const userExists = await User.findOne({ email });
-  
-            if (!userExists) {
-              const bookmarks: [] = [];
-              const res = await fetch('http://localhost:3000/api/user', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  name,
-                  email,
-                  bookmarks
-                }),
-              });
-  
-              if (res.ok) {
-                // return user;
-                return true;
-              }
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === 'google') {
+        const { name, email } = user;
+        try {
+          await connectMongoDB();
+          const userExists = await User.findOne({ email });
+
+          if (!userExists) {
+            const bookmarks: [] = [];
+            const res = await fetch('http://localhost:3000/api/user', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                name,
+                email,
+                bookmarks,
+              }),
+            });
+
+            if (res.ok) {
+              // return user;
+              return true;
             }
-          } catch (error) {
-            console.log(error);
           }
+        } catch (error) {
+          console.log(error);
         }
-  
-        // return user;
-        return true;
-      },
+      }
+
+      // return user;
+      return true;
     },
-  };
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
+  },
+  pages: {
+    signIn: '/connexion',
+  },
+};
